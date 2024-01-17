efcdd1b76921ebb7e4221b443bc024b9baacc761
Aarya R <aarya.masters@gmail.com>
2024-01-17 22:25:53 +0530

    doing event stuff in aarya

--- frontend/src/components/Events.js
+++ frontend/src/components/Events.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Auroractf } from './Auroractf'
+import DevsprintEvents from './DevsprintEvents'
 import Timeline from './timeline/Timeline'
 
 export default function Events() {
   return (
-    <div>
+    <div className='bg-[#000509]'>
       <h1 className='text-center'>EVENTS</h1>
 
       <Timeline/>
+
+      <DevsprintEvents/>
+
+      <Auroractf/>
     </div>
   )
 }
