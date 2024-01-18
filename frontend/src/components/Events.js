3beacf4ef139bf3e221c7b80c77ece8649ea8012
Aarya R <aarya.masters@gmail.com>
2024-01-18 11:18:31 +0530

    events page 90% done

--- frontend/src/components/Events.js
+++ frontend/src/components/Events.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import events from '../images/events.svg';
+import eventslines from '../images/eventslines.svg';
 import { Auroractf } from './Auroractf';
 import DevsprintEvents from './DevsprintEvents';
 import TimelineEvents from './timeline/TimelineEvents';
@@ -6,10 +8,17 @@ import TimelineEvents from './timeline/TimelineEvents';
 export default function Events(){
   return (
     <div className='bg-[#000509]'>
+      
+
+      <img src={events} alt="" className='mx-auto'/>
 
       <TimelineEvents/>
 
+      <img src={eventslines} alt="" className='mx-auto'/>
+
       <DevsprintEvents/>
+      
+      <img src={eventslines} alt="" className='mx-auto'/>
 
       <Auroractf/>
     </div>
